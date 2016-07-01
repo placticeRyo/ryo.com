556ecf36c3bc5f6e58e0a7a4cda23a2bc041b48a
佐山絢 <sayamaken0402@gmail.com>
2016-07-01 21:45:42 +1000

    I'm sorry

--- js/common.js
+++ js/common.js
@@ -1,6 +1,18 @@
-$(function() {
-  $(window).scroll(function() {
-    var scrT = $(window).scrollTop();
-    console.log(scrT);
+$(function(){
+  var windowH = $(window).height();
+  var headerH = $("#header").height();
+  var footerH = $("#footer").height();
+  var contentH = windowH - footerH - headerH;
+
+
+  $(function(){
+    $("#main").backstretch([
+      "./image/image1.jpeg",
+      "./image/image2.jpeg",
+      "./image/image3.jpeg",
+      "./image/image4.jpeg"
+    ], {duration : 3000, fade: 2000
+    });
   });
+
 });
