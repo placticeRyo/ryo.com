19226c1e7fc11ff6f04fcf35c97e74b86a2ca84c
佐山絢 <sayamaken0402@gmail.com>
2016-07-06 22:08:26 +1000

    バグ修正

--- meta.php
+++ meta.php
@@ -1,10 +1,10 @@
 <meta charset="UTF-8">
 <title>Ryo.com</title>
-<link rel="stylesheet" href="./css/reset.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
 <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 <link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
-<script src="./js/jquery.min.js" charset="utf-8"></script>
-<script src="./js/footerFixed.js" charset="utf-8"></script>
-<script src="./js/backstretch.js"></script>
-<script src="./js/common.js" charset="utf-8"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js" charset="utf-8"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/footerFixed.js" charset="utf-8"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/backstretch.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/common.js" charset="utf-8"></script>
